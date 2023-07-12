fc965b9e6310dd08a866e62ef35ca8c452cd01de
Hector Chocobar-Torrejon <45291267+hchocobar@users.noreply.github.com>
2023-07-12 11:38:46 +0000

    review: Delete divs

--- src/js/component/ControlledInput.jsx
+++ src/js/component/ControlledInput.jsx
@@ -14,36 +14,34 @@ const ControlledInput = () => {
   };
 
   return (
-    <div className="form-control mt-5">
-      <form onSubmit={handleSubmit}>
-        <h2>Multiples Inputs</h2>
-        <div className="mb-3">
-          <label>Your name</label>
-          <input type='text' className="form-control" 
-            value={name}
-            onChange={(e) => setName(e.target.value)}/>
-        </div>
-        <div className="mb-3">
-          <label>Choose your country</label>
-          <select className="form-control"
-                  value={country}
-                  onChange={(e) => {setCountry(e.target.value)}}>
-            <option value='ca'>Canada</option>
-            <option value='es'>Spain</option>
-            <option value='ua'>Ukraine</option>
-            <option value='gb'>United Kingdom</option>
-            <option value='us'>United State</option>
-            <option value='uy'>Uruguay</option>
-          </select>
-        </div>
-        <div className="mb-2">
-          <input type='checkbox' className="form-check-input me-3"
-            checked={isActive}
-            onChange={() => setIsActive(!isActive)}/>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </div>
-      </form>
-    </div>    
+    <form onSubmit={handleSubmit} className="form-control mt-5">
+      <h2>Multiples Inputs</h2>
+      <div className="mb-3">
+        <label>Your name</label>
+        <input type='text' className="form-control"
+          value={name}
+          onChange={(e) => setName(e.target.value)} />
+      </div>
+      <div className="mb-3">
+        <label>Choose your country</label>
+        <select className="form-control"
+          value={country}
+          onChange={(e) => { setCountry(e.target.value) }}>
+          <option value='ca'>Canada</option>
+          <option value='es'>Spain</option>
+          <option value='ua'>Ukraine</option>
+          <option value='gb'>United Kingdom</option>
+          <option value='us'>United State</option>
+          <option value='uy'>Uruguay</option>
+        </select>
+      </div>
+      <div className="mb-2">
+        <input type='checkbox' className="form-check-input me-3"
+          checked={isActive}
+          onChange={() => setIsActive(!isActive)} />
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </div>
+    </form>
   )
 };
 
